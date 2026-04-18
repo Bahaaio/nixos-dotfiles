b3cba3f0866dcf6b80a56260daae7b9e30ce4690
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-18 19:40:07 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "living on the edge";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
