4406dcc0ee06585582287af0fb60e521a332942a
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-19 23:32:45 +0200

    feat: add power module
    - add tlp
    - configure logind

--- modules/default.nix
+++ modules/default.nix
@@ -4,6 +4,7 @@
     ./cli.nix
     ./dev.nix
     ./docker.nix
+    ./power.nix
     ./audio.nix
     ./networking.nix
   ];
---
+++ modules/power.nix
@@ -0,0 +1,47 @@
+{ ... }:
+
+{
+  services.logind = {
+    settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandlePowerKey = "suspend";
+      HandlePowerKeyLongPress = "reboot";
+    };
+  };
+
+  services.tlp = {
+    enable = true;
+
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 80;
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_HWP_ON_AC = 1;
+      CPU_HWP_ON_BAT = 0;
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      # PLATFORM_PROFILE_ON_BAT = "balanced";
+
+      SOUND_POWER_SAVE_ON_AC = 0;
+      # SOUND_POWER_SAVE_ON_BAT = 1;
+
+      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+
+      # helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 60;
+    };
+  };
+}
