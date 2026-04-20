9f2c5b8fb6d25f2ed41fe64511aba8dca05193df
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-20 21:15:10 +0200

    feat: add sddm, grim & slurp & xdph

--- modules/wm.nix
+++ modules/wm.nix
@@ -1,7 +1,10 @@
 { inputs, pkgs, ... }:
 
 {
-  services.displayManager.gdm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
 
   programs = {
     hyprland = {
@@ -11,6 +14,15 @@
     };
 
     firefox.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+  };
+
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
   };
 
   environment.systemPackages = with pkgs; [
@@ -30,6 +42,8 @@
     copyq
     dunst
     wlogout
+    grim
+    slurp
 
     libreoffice
     localsend
@@ -38,6 +52,7 @@
     zapzap
     ticktick
     obs-studio
+    qt6.qtwayland
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
 
     # dev
@@ -49,4 +64,16 @@
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
+
+  # programs.dconf.profiles.user.databases = [
+  #   {
+  #     settings."org/gnome/desktop/interface" = {
+  #       gtk-theme = "Adwaita";
+  #       icon-theme = "Flat-Remix-Red-Dark";
+  #       font-name = "Noto Sans Medium 11";
+  #       document-font-name = "Noto Sans Medium 11";
+  #       monospace-font-name = "Noto Sans Mono Medium 11";
+  #     };
+  #   }
+  # ];
 }
