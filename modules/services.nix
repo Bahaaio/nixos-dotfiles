6979df344b2581565d4c757460d103eb171bb1b9
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-21 01:18:32 +0200

    feat: add gnome-keyring and polkit-gnome

--- modules/services.nix
+++ modules/services.nix
@@ -8,9 +8,27 @@
     gvfs.enable = true; # Mount, trash, network drives
     udisks2.enable = true; # USB drives, disks
     devmon.enable = true;
+    gnome.gnome-keyring.enable = true; # Keyring for storing secrets
+  };
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
 
   environment.systemPackages = with pkgs; [
     polkit_gnome
+    gnome.gvfs
   ];
 }
