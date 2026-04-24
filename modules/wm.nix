9c4620893daf9aaf7711349f6b23a6ec54d09727
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-25 01:44:54 +0300

    feat: add gnome-weather & walker bin cache

--- modules/wm.nix
+++ modules/wm.nix
@@ -55,8 +55,9 @@
     satty
     pavucontrol
     swayosd
-    gnome-calculator
 
+    gnome-weather
+    gnome-calculator
     libreoffice
     localsend
     obsidian
@@ -81,5 +82,17 @@
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "wezterm";
+  };
+
+  # walker binary cache
+  nix.settings = {
+    extra-substituters = [
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+    ];
   };
 }
