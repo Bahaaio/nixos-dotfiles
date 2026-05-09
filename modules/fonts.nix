8616b69af001c521808ee5a0bf6e33605ff9d58a
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-10 02:13:38 +0300

    feat(fonts): add Inter & icon fonts
    - remvove fira-sans
    - add inter as default sansSerif
    - add noto-fonts-cjk
    - add font-awesome and material-icons

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,15 +5,23 @@
     enableDefaultPackages = true;
 
     packages = with pkgs; [
-      fira-sans
+      inter
       noto-fonts
+      noto-fonts-cjk-sans
       noto-fonts-color-emoji
+
+      # nerd fonts
+      nerd-fonts.symbols-only
       nerd-fonts.jetbrains-mono
+
+      # icon fonts
+      font-awesome
+      material-design-icons
     ];
 
     fontconfig.defaultFonts = {
       serif = [ "Noto Serif" ];
-      sansSerif = [ "Noto Sans" ];
+      sansSerif = [ "Inter" ];
       monospace = [ "JetBrainsMono Nerd Font" ];
       emoji = [ "Noto Color Emoji" ];
     };
