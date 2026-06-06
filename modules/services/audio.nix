3e9b2f38716df9d13d823530c0f15cb97f5463ec
Bahaa Mohamed <bahaa-dev@proton.me>
2026-06-07 00:07:04 +0300

    feat(audio): add deepfilternet ladspa plugin
    - finally getting a clean voice :)

--- modules/services/audio.nix
+++ modules/services/audio.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   security.rtkit.enable = true;
@@ -8,5 +8,68 @@
     pulse.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+
+    extraLadspaPackages = [ pkgs.deepfilternet ];
+
+    extraConfig.pipewire = {
+      "10-disable-x11-bell" = {
+        "context.properties" = {
+          "module.x11.bell" = false;
+        };
+      };
+
+      "20-rt-latency" = {
+        "context.properties" = {
+          "default.clock.quantum" = 2048;
+          "default.clock.min-quantum" = 1024;
+        };
+        "context.modules" = [
+          {
+            name = "libpipewire-module-rt";
+            flags = [ "nofail" ];
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = 200000;
+              "rt.time.hard" = 200000;
+            };
+          }
+        ];
+      };
+
+      # https://github.com/Rikorose/DeepFilterNet/blob/main/ladspa/filter-chain-configs/deepfilter-mono-source.conf
+      "99-deepfilter-mic" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-filter-chain";
+            flags = [ "nofail" ];
+            args = {
+              "node.description" = "DeepFilter Noise Canceling source";
+              "media.name" = "DeepFilter Noise Canceling source";
+
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "DeepFilter Mono";
+                    plugin = "libdeep_filter_ladspa";
+                    label = "deep_filter_mono";
+                    control = {
+                      "Attenuation Limit (dB)" = 100;
+                    };
+                  }
+                ];
+              };
+
+              "audio.rate" = 48000;
+              "audio.position" = [ "MONO" ];
+
+              "capture.props"."node.passive" = true;
+              "playback.props"."media.class" = "Audio/Source";
+            };
+          }
+        ];
+      };
+    };
   };
 }
